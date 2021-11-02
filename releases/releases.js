3ba2524ed8863ebd66a73d76cdbce715451fd0d9
Drk <38827141+drpidman@users.noreply.github.com>
2021-11-02 00:56:04 -0300

    pagination

--- releases/releases.js
+++ releases/releases.js
@@ -4,35 +4,40 @@ let gitReleasesUrl = "https://api.github.com/repos/Drkryz/drkryz.github.io/relea
 
 
 async function load() {
-    const root = document.getElementById("releases-card");
+    const root = document.getElementById("releases-card-root");
     const releases = await getReleases();
-    console.log(releases);
 
-    releases.map(release => {
-        let htmlTemplate = `
-        <div class="card">
-            <div class="card-top">
-                <h1>${release.name}</h1>
-            </div>
-            <div class="card-body">
-                <span>${release.body}</span>
-            </div>
-            <div class="card-bottom">
-                <a class="link-button" href=${release.assets[0].browser_download_url}>
-                    <i class="fas fa-download"></i>
-                    <span>Download</span>
-                </a>
-                <a class="link-button" href=${release.html_url}>
-                    <i class="fas fa-info-circle"></i>
-                    <span>Informações</span>
-                </a>
-            </div>
-        </div>
-        `;
-
-
-        root.innerHTML += htmlTemplate;
-    });
+    $("#releases-card").pagination({
+        dataSource: releases,
+        showNavigator: true,
+        pageSize: 5,
+        callback: function (data, pagination) {
+            let htmlTemplate = ``;
+            data.map(release => {
+                htmlTemplate += `
+                <div class="card">
+                    <div class="card-top">
+                        <h1>${release.name}</h1>
+                    </div>
+                    <div class="card-body">
+                        <span>${release.body}</span>
+                    </div>
+                    <div class="card-bottom">
+                        <a class="link-button" href=${release.assets[0].browser_download_url}>
+                            <i class="fas fa-download"></i>
+                            <span>Download</span>
+                        </a>
+                        <a class="link-button" href=${release.html_url}>
+                            <i class="fas fa-info-circle"></i>
+                            <span>Informações</span>
+                        </a>
+                    </div>
+                </div>
+                `;
+            });
+            root.innerHTML = htmlTemplate;
+        }
+    })
 
 }
 
